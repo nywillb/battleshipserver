5b4a5307e6c3c81875b7c9d141c46da2209a1bf1
William B <william@barkoffusa.com>
2017-10-28 21:27:25 -0400

    fix array indexing errors

--- index.js
+++ index.js
@@ -9,17 +9,19 @@ var boards = {};
 // The function passed to net.createServer() becomes the event handler for the 'connection' event
 // The sock object the callback function receives UNIQUE for each connection
 net.createServer(function (sock){
+    sock.setNoDelay(true);
 
     var currentgamecode = null;
     var playernumber = null;
 
     // We have a connection - a socket object is assigned to the connection automatically
-    // console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
+     console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
 
     // Add a 'data' event handler to this instance of socket
     sock.on('data', function (data) {
+        
 
-        // console.log('DATA ' + sock.remoteAddress + ': ' + data);
+        console.log('DATA ' + sock.remoteAddress + ': ' + data);
 
         msg = data.toString('utf8').trim()
         if (msg === "GETGAMECODE") {
@@ -35,7 +37,7 @@ net.createServer(function (sock){
                 }
             }
             // gamecodes[gamecodes.length++] = gamecode
-            sock.write('GAMECODE ' + gamecode);
+            sock.write('GAMECODE ' + gamecode + "\n");
             currentgamecode = gamecode;
             playernumber = 1;
         }
@@ -43,12 +45,12 @@ net.createServer(function (sock){
             res = msg.split(" ");
             if (games[res[1]] != null) { //game exists!
                 games[res[1]].player2.socket = sock;
-                sock.write("JOIN success");
-                games[res[1]].player1.socket.write("JOIN success");
+                sock.write("JOIN success\n");
+                games[res[1]].player1.socket.write("JOIN success\n");
                 currentgamecode = res[1];
                 playernumber = 2;
             } else {
-                sock.write("JOIN fail");
+                sock.write("JOIN fail\n");
             }
         }
         if (currentgamecode != null) {
@@ -67,19 +69,19 @@ net.createServer(function (sock){
                 // console.log(games[currentgamecode]["player" + playernumber].board);
                 if (playernumber == 1) {
                     if (games[currentgamecode].player2.board == null) {
-                        games[currentgamecode].player2.socket.write("PLAYERREADY");
-                        games[currentgamecode].player1.socket.write("PLAYERREADY");
+                        games[currentgamecode].player2.socket.write("PLAYERREADY\n");
+                        games[currentgamecode].player1.socket.write("PLAYERREADY\n");
                     } else {
-                        games[currentgamecode].player2.socket.write("MOVEFIRST no");
-                        games[currentgamecode].player1.socket.write("MOVEFIRST yes");
+                        games[currentgamecode].player2.socket.write("MOVEFIRST no\n");
+                        games[currentgamecode].player1.socket.write("MOVEFIRST yes\n");
                     }
                 } else {
                     if (games[currentgamecode].player1.board == null) {
-                        games[currentgamecode].player1.socket.write("PLAYERREADY");
-                        games[currentgamecode].player2.socket.write("PLAYERREADY");
+                        games[currentgamecode].player1.socket.write("PLAYERREADY\n");
+                        games[currentgamecode].player2.socket.write("PLAYERREADY\n");
                     } else {
-                        games[currentgamecode].player2.socket.write("MOVEFIRST no");
-                        games[currentgamecode].player1.socket.write("MOVEFIRST yes");
+                        games[currentgamecode].player2.socket.write("MOVEFIRST no\n");
+                        games[currentgamecode].player1.socket.write("MOVEFIRST yes\n");
                     }
                 }
             } else if (msg.includes("MOVE")) {
@@ -87,29 +89,29 @@ net.createServer(function (sock){
                 if (res[1] == "surrender") {
                     sock.write("MOVE surrendered")
                     if (playernumber === 1) {
-                        games[currentgamecode].player2.socket.write("MOVE surrendered");
+                        games[currentgamecode].player2.socket.write("MOVE surrendered\n");
                     } else {
-                        games[currentgamecode].player1.socket.write("MOVE surrendered");
+                        games[currentgamecode].player1.socket.write("MOVE surrendered\n");
                     }
                     delete games[currentgamecode];
                 } else {
                     if (playernumber === 1) {
-                        if (games[currentgamecode].player2.board[res[1]+1][res[2]+1] === 's') {
-                            games[currentgamecode].player1.socket.write("MOVE hit" + (res[1]+1) + " " + res[2]);
-                            games[currentgamecode].player2.socket.write("MOVE hit" + (res[1]+1) + " " + res[2]);
+                        if (games[currentgamecode].player2.board[res[1]][res[2]] === 's') {
+                            games[currentgamecode].player1.socket.write("MOVE hit" + (res[1]) + " " + res[2]+"\n");
+                            games[currentgamecode].player2.socket.write("MOVE hit" + (res[1]) + " " + res[2]+"\n");
                             games[currentgamecode].player2.board[res[1]][res[2]] = "x";
                         } else if (games[currentgamecode].player2.board[res[1]][res[2]] === 'x') {
-                            games[currentgamecode].player1.socket.write("MOVE miss" + res[1] + " " + res[2]);
-                            games[currentgamecode].player2.socket.write("MOVE miss" + res[1] + " " + res[2]);
+                            games[currentgamecode].player1.socket.write("MOVE miss" + res[1] + " " + res[2]+"\n");
+                            games[currentgamecode].player2.socket.write("MOVE miss" + res[1] + " " + res[2]+"\n");
                         }
                     } else {
                         if (games[currentgamecode].player1.board[res[1]][res[2]] === 's') {
-                            games[currentgamecode].player1.socket.write("MOVE hit" + res[1] + " " + res[2]);
-                            games[currentgamecode].player2.socket.write("MOVE hit" + res[1] + " " + res[2]);
+                            games[currentgamecode].player1.socket.write("MOVE hit" + res[1] + " " + res[2]+"\n");
+                            games[currentgamecode].player2.socket.write("MOVE hit" + res[1] + " " + res[2]+"\n");
                             games[currentgamecode].player1.board[res[1]][res[2]] = "x";
                         } else if (games[currentgamecode].player1.board[res[1]][res[2]] === 'x') {
-                            games[currentgamecode].player1.socket.write("MOVE miss" + res[1] + " " + res[2]);
-                            games[currentgamecode].player2.socket.write("MOVE miss" + res[1] + " " + res[2]);
+                            games[currentgamecode].player1.socket.write("MOVE miss" + res[1] + " " + res[2]+"\n");
+                            games[currentgamecode].player2.socket.write("MOVE miss" + res[1] + " " + res[2]+"\n");
                         }
                     }
                     var player1hasremaining = null;
@@ -125,18 +127,18 @@ net.createServer(function (sock){
                         }
                     }
                     if (!player1hasremaining && player2hasremaining) {
-                        games[currentgamecode].player1.socket.write("GAMEEND lose");
-                        games[currentgamecode].player2.socket.write("GAMEEND win");
+                        games[currentgamecode].player1.socket.write("GAMEEND lose\n");
+                        games[currentgamecode].player2.socket.write("GAMEEND win\n");
                         delete games[currentgamecode];
                     } else if (player1hasremaining && !player2hasremaining) {
-                        games[currentgamecode].player1.socket.write("GAMEEND win");
-                        games[currentgamecode].player2.socket.write("GAMEEND lose");
+                        games[currentgamecode].player1.socket.write("GAMEEND win\n");
+                        games[currentgamecode].player2.socket.write("GAMEEND lose\n");
                         delete games[currentgamecode];
                     }
                 }
             }
         } else {
-            sock.write("ERROR \"first make a game, then try to play\"")
+            sock.write("ERROR \"first make a game, then try to play\"\n")
         }
     })
 
